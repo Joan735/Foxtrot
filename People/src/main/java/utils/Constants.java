2eeb7c349477baa4b2d5f1c612b831276ffef9a5
Rorro101004 <rodrigofernando.cg@gmail.com>
2025-04-29 11:54:42 +0200

    issue #2 Constants added

---
+++ People/src/main/java/utils/Constants.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author roroc
+ */
+public class Constants {
+
+    public static final String arrayList = "ArrayList";
+    public static final String hashMap = "HashMap";
+    public static final String file = "File";
+    public static final String serialization = "File (Serialization):";
+    public static final String SQL = "SQL - Database";
+    public static final String JPA = "JPA - Database";
+    
+
+
+}
